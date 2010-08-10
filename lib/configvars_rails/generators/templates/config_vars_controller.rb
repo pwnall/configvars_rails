cfead0ae147ed5958a6699df1e31ea55b0446046
Victor Costan <costan@gmail.com>
2010-08-10 12:58:24 -0400

    Added config var for HTTP basic auth realm.

--- lib/configvars_rails/generators/templates/config_vars_controller.rb
+++ lib/configvars_rails/generators/templates/config_vars_controller.rb
@@ -6,9 +6,10 @@ class ConfigVarsController < ApplicationController
   # around modifying them.
   before_filter :http_basic_check
   def http_basic_check
-    authenticate_or_request_with_http_basic do |user, password|
+    authenticate_or_request_with_http_basic(
+        ConfigVar['config_vars.http_realm']) do |user, password|
       user == ConfigVar['config_vars.http_user'] &&
-      password == ConfigVar['config_vars.http_password']
+          password == ConfigVar['config_vars.http_password']
     end
   end
   private :http_basic_check
